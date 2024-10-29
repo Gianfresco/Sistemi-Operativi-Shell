2042ec39d90dea8f111a5fd8152970959140dbe7
gianfresco <gianfranco.cavallini@edu.unife.it>
2024-10-29 10:53:26 +0100

    2021-04-26 riscritto

--- 2021-04-26/cerca_misure.sh
+++ 2021-04-26/cerca_misure.sh
@@ -1,42 +1,33 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
-if test "$#" -lt 3
-then
-    echo "ERRORE - Uso: $0 <dir> <header>"
+if test ! "$#" -ne 2
+then 
+    echo "Uso: $0 <dir> <header>"
     exit 1
 fi
 
-dir="$1";       shift
-header="$2";   shift
+dir=$1
+shift
+header=$2
+shift
 
 case "$dir" in
-    /*) ;;
-    *)  echo "<dir> deve essere un percorso assoluto"
-        exit 2;;
+    /*) if test ! -d "$1" -o ! -x "$1"
+        then
+            echo "$dir non è una directory o non si hanno i permessi necessari"
+            exit 2
+        fi ;;
+    *)  echo "$dir deve essere un percorso assoluto"
+        exit 3;;
 esac
 
-if test ! -d "$dir"
-then
-    echo "Errore: $dir non è una directory"
-    exit 3
-fi
-
-if test ! -x "$dir"
-then
-    echo "Errore: non è posissibile eseguire $dir"
-    exit 4
-fi
-
-PATH=$PATH:$(pwd)
+PATH=$PATH:`pwd`
 export PATH
 
 echo "" > /tmp/.dir.tmp
-echo "0" > /tmp/.counter.tmp
-echo "" > "$HOME"/misure.txt
+echo "0" > /tmp/.count.tmp
 
 sh cerca_misure_ric.sh "$dir" "$header"
 
 cat /tmp/.dir.tmp
-cat /tmp/.counter.tmp
-
-rm -f /tmp/.dir.tmp /tmp/.counter.tmp
+cat /tmp/.count.tmp
--- 2021-04-26/cerca_misure_ric.sh
+++ 2021-04-26/cerca_misure_ric.sh
@@ -1,33 +1,35 @@
 #!/bin/sh
 
-dir="$1";       shift
-header="$2";    shift
+dir=$1
+shift
+header=$2
+shift
 
 cd "$dir" || exit 1
 
 counter=0
 for f in *.log
 do
-    if test -f "$f" -a -w "$f"
+    if test -r "$f" -a -w "$f"
     then
-        if test "$(head -n 1 "$f" | grep -c "$header")" -ge 1
+        if test `head -n 1 | grep "$header"` -ge 1
         then
-            echo "$(pwd)"/"$f" >> "$HOME"/misure.txt
-            counter=$(("$counter" + 1))
+            echo "`pwd`/$f" >> "$HOME"/misure.txt
+            counter=`expr "$counter" + 1`
         fi
     fi
 done
 
-if test "$counter" -gt "$(cat /tmp/.counter.tmp)"
+if test "$counter" -gt `cat /tmp/.count.tmp`
 then
-    echo "$dir">/tmp/.dir.tmp
-    echo "$counter">/tmp/.counter.tmp
+    echo "$counter" > /tmp/.count.tmp
+    echo "$dir" > /tmp/.dir.tmp
 fi
 
-for d in *
+for d in "$dir"
 do
     if test -d "$d" -a -x "$d"
     then
-        cerca_misure_ric.sh "$(pwd)"/"$d" "$header"
+        sh ricerca_misure.sh `pwd`/"$d" "$header"
     fi
 done
